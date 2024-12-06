68c4e822f09194e5b645e183dabb773cda81d571
Pavel Smolyantsev <pavel.smolyantsev@mail.ru>
2024-12-06 19:45:43 +0000

    Advance on input error checking

--- src/lab1.c
+++ src/lab1.c
@@ -1,10 +1,10 @@
 #include "lab1.h"
 
 int main(int argc, char **argv) {
-    if ((argc % 3) != 0)
+    int inputValidationResultCode = validateInput(argc, argv);
+    if (inputValidationResultCode > 0)
     {
-        printf("Invalid input\n");
-        return 0;
+        return inputValidationResultCode;
     }
 
     int results[argc / 3];
@@ -27,10 +27,72 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+int validateInput(int argc, char** argv)
+{
+    int code = 0;
+    if (argc < 6)
+    {
+        printf("Не пройдена проверка: должно быть минимум 6 аргументов.\n");
+        code = 1;
+    }
+    else if ((argc % 3) != 0)
+    {
+        printf("Не пройдена проверка: кол-во аргументов должно быть кратно 3.\n");
+        code = 1;
+    }
+    else if (!isNumber(argv[argc - 1]))
+    {
+        printf("Не пройдена проверка: последний аргумент должен быть числом.\n");
+        code = 1;
+    }
+    else if (strcmp(argv[argc - 2], "-k") != 0)
+    {
+        printf("Не пройдена проверка: предпоследний аргумент должен быть -k.\n");
+        code = 1;
+    }
+
+
+    for (int i = 1; argc; i += 3)
+    {
+        if (i == argc - 2)
+        {
+            break;
+        }
+        if (!isNumber(argv[i]) || !isNumber(argv[i + 2]) || !isCharacterInString(argv[i + 1], "+-*%"))
+        {
+            printf("Не пройдена проверка: первый и третий аргумент в парах - числа, средний - поддерживаемая операция.\n");
+            code = 1;
+            break;
+        }
+    }
+
+    if (code != 0)
+    {
+        printf("Ошибка вводных данных.\n");
+    }
+    return code;
+}
+
+int isCharacterInString(char operation, char* potentialOperations)
+{
+    return strchr(potentialOperation, operation) != NULL;
+}
+
+int isNumber(char* potentialNumber)
+{
+    while (*potentialNumber) {
+        if (!isdigit(*potentialNumber)) {
+            return 0;
+        }
+        potentialNumber++;
+    }
+    return 1;
+}
+
 void printResult(int* results, int size)
 {
     printf("Результирующая строка: ");
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size - 1; ++i)
     {
         printf("%c", results[i]);
     }
--- src/lab1.h
+++ src/lab1.h
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void printResult(int* results, int size);
 
 int calculate(char* aChar, char* operation, char* bChar);
+
+int validateInput(int argc, char** argv);
+
+int isNumber(char* potentialNumber);
