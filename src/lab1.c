c0a8deae80127596ebbb5bac01a3a40b4548a152
Pavel Smolyantsev <pavel.smolyantsev@mail.ru>
2024-12-06 18:01:14 +0000

    Print pairs

--- src/lab1.c
+++ src/lab1.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
- 
-int main(int argc, char **argv) { 
- 
-    printf("%d\n", argc);
- 
-    for (int i = 0; i < argc; i++)
-        puts(argv[i]);
+
+int main(int argc, char **argv) {
+    if ((argc % 3) != 0)
+    {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (int i = 1; argc; i += 3)
+    {
+        if (i == argc - 2)
+        {
+            break;
+        }
+        printf("%s %s %s\n", argv[i], argv[i + 1], argv[i + 2]);
+    }
+    return 0;
 }
