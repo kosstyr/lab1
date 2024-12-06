18b80791d8663652743a6d7a198f08a1bfa9ee36
Pavel Smolyantsev <pavel.smolyantsev@mail.ru>
2024-12-06 20:56:10 +0000

    Last version

--- src/lab1.c
+++ src/lab1.c
@@ -1,14 +1,23 @@
 #include "lab1.h"
 
+//argc (argument count) - кол-во аргументов
+//argv (argument values) - массив со значениями аргументов
+//первый аргумент (argv[0]) - всегда название исполняемого файла, поэтому почти везде начинаем со второго элемента
+//последний аргумент - argv[argc - 1]
 int main(int argc, char **argv) {
     int inputValidationResultCode = validateInput(argc, argv);
-    if (inputValidationResultCode > 0)
+    if (inputValidationResultCode != 0)
     {
         return inputValidationResultCode;
     }
 
+    //Создаём массив размером кол-во аргументов / 3, чтобы хранить результаты
+    //каждой комбинации число операция число
     int results[argc / 3];
     int key = atoi(argv[argc - 1]);
+    //начинем проходиться по массиву
+    //пропускаем нулевой аргумент (название исполняемого файла)
+    //идём с шагом 3 (первое число, операция, второе число, _следующее первое число_...)
     for (int i = 1; i < argc - 2; i += 3)
     {
         //printf("%s %s %s\n", argv[i], argv[i + 1], argv[i + 2]);
@@ -28,7 +37,7 @@ int validateInput(int argc, char** argv)
     int code = 0;
     if (argc < 6)
     {
-        printf("Не пройдена проверка: должно быть минимум 6 аргументов.\n");
+        printf("Не пройдена проверка: должно быть минимум 5 аргументов.\n");
         code = 1;
     }
     else if ((argc % 3) != 0)
@@ -47,20 +56,23 @@ int validateInput(int argc, char** argv)
         code = 1;
     }
 
-
-    for (int i = 1; i < argc - 2; i += 3)
+    if (code == 0)
     {
-        if ((strcmp(argv[i + 1], "%") == 0) && (strcmp(argv[i + 2], "0") == 0))
+        for (int i = 1; i < argc - 2; i += 3)
         {
-            printf("Не пройдена проверка: деление на 0.\n");
-            code = 1;
-            break;
-        }
-        else if (!isNumber(argv[i]) || !isNumber(argv[i + 2]) || !isCharacterInString(argv[i + 1][0], "+-*%"))
-        {
-            printf("Не пройдена проверка: первый и третий аргумент в парах - числа, средний - поддерживаемая операция.\n");
-            code = 1;
-            break;
+            //strcmp - функция сравнения строк, если строки равны, возвращает 0
+            if ((strcmp(argv[i + 1], "%") == 0) && (strcmp(argv[i + 2], "0") == 0))
+            {
+                printf("Не пройдена проверка: деление на 0.\n");
+                code = 1;
+                break;
+            }
+            else if (!isNumber(argv[i]) || !isNumber(argv[i + 2]) || !isCharacterInString(argv[i + 1][0], "+-*%"))
+            {
+                printf("Не пройдена проверка: первый и третий аргумент в парах - числа, средний - поддерживаемая операция.\n");
+                code = 1;
+                break;
+            }
         }
     }
 
@@ -73,6 +85,8 @@ int validateInput(int argc, char** argv)
 
 int isCharacterInString(char operation, char* potentialOperations)
 {
+    //strchr - функция проверки наличия символа в строке
+    //если возвращает NULL, значит, символа в строке нет
     if (strchr(potentialOperations, operation) != NULL)
     {
         return 1;
@@ -86,6 +100,8 @@ int isNumber(char* potentialNumber)
     {
         potentialNumber++;
     }
+
+    //пока указатель имеет значение
     while (*potentialNumber) {
         if (!isdigit(*potentialNumber)) {
             return 0;
